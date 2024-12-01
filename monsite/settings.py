2e92d2395249735900e6deb688d29c7a2244febf
bangri <bikoyemmanuel531@gmail.com>
2024-12-01 05:01:48 +0100

    debug false

--- monsite/settings.py
+++ monsite/settings.py
@@ -15,8 +15,6 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = True
-# ALLOWED_HOSTS = ['*']
 DEBUG = False
 ALLOWED_HOSTS = ['bangri-agtech.onrender.com', '127.0.0.1', 'localhost']
 
--- monsite/urls.py
+++ monsite/urls.py
@@ -22,6 +22,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG is False:
+# if settings.DEBUG is False:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += staticfiles_urlpatterns()
