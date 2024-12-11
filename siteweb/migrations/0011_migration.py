85dbbb162087774e65b9a357fd6e1bc5c7afa6be
bangri <bikoyemmanuel531@gmail.com>
2024-12-11 05:35:27 +0100

    change img profil

--- siteweb/migrations/0011_migration.py
+++ siteweb/migrations/0011_migration.py
@@ -1,10 +1,8 @@
 from django.db import migrations
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('bangri-agritech', '0010_ebook.py'),
-    ]
+]
 
     operations = [
         migrations.RunSQL("CREATE EXTENSION IF NOT EXISTS pg_trgm;"),
